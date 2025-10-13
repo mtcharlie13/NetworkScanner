06ca63095e233daa4e1b88fc0ac2fd9b45771637
mtcharlie13 <charlie.tks13@gmail.com>
2025-10-14 00:03:30 +0200

    Added functionality to ping a host

--- scanner.py
+++ scanner.py
@@ -1,8 +1,12 @@
 import ipaddress
 import os
+import socket
 import struct
 import sys
 import time
+import traceback
+
+# TODO ckeck administrator privileges are enabled
 
 # calculate checksum
 def get_checksum(data):
@@ -11,8 +15,8 @@ def get_checksum(data):
 
     checksum = 0
     for i in range(0, len(data), 2):
-        data = (data[i] << 8) + data[i + 1]# combine data into 16-bit word
-        checksum += data
+        word = (data[i] << 8) + data[i + 1]# combine data into 16-bit word
+        checksum += word
         checksum = (checksum & 0xffff) + (checksum >> 16)
 
     # invert bits and return 16 bits
@@ -25,32 +29,83 @@ class ICMPPing:
         self.timeout = 2
         self.sequence_num = 0
 
-        packet = self.create_packet
+        packet = self.create_packet()
+
+        self.ping_host(target_addr, packet)
 
     # create ICMP packet
     def create_packet(self):
         header = struct.pack('!BBHHH', 8, 0, 0, self.packet_ID, self.sequence_num)
 
-        payload = struct.pack('!d', time.time())# record time sent
-
-        extra_data_len = 48# length needed to reach 64 bytes
+        extra_data_len = 56# length needed to reach 64 bytes
         extra_data = str.encode(extra_data_len * 'A')
 
-        checksum = get_checksum(header + payload + extra_data)
+        checksum = get_checksum(header + extra_data)
 
         # reconstruct packet with checksum
         header = struct.pack('!BBHHH', 8, 0, checksum, self.packet_ID, self.sequence_num)
 
-        return header + payload + extra_data
+        return header + extra_data
 
-    # send ICMP Echo Request to target
-    # def send_request(self, target_addr):
+    # set up socket and ping host specified by address
+    def ping_host(self, target_addr, packet):
+        try:
+            dst_addr = socket.gethostbyname(target_addr)
+        except socket.gaierror as e:
+            print('Error: cannot resolve hostname %s' % (target_addr))
 
-    # receive ICMP Echo Reply from target
-    # def receive_reply(self, target_addr):
+        # create ICMP socket and set timeout
+        try:
+            icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+        except socket.error as e:
+            traceback.print_exception(e)
+            return
 
-    # ping host specified by address
-    # def ping_host(self, target_addr):
+        icmp_socket.settimeout(self.timeout)
+
+        # send ICMP Echo Request to target
+        try:
+            bytes_sent = icmp_socket.sendto(packet, (dst_addr, 0))
+            time_sent = time.time()
+            print('Sent %d bytes to %s' % (bytes_sent, dst_addr))
+        except socket.error as e:
+            traceback.print_exception(e)
+            icmp_socket.close()
+            return
+
+        # receive ICMP Echo Reply from target and record rtt
+        reply_packet = None
+
+        try:
+            reply_packet, src_addr = icmp_socket.recvfrom(1024)# read up to 1024 bytes from socket
+            time_received = time.time()
+            rtt = time_received - time_sent
+        except socket.timeout:
+            print('Request timed out')
+            return
+        except socket.error as e:
+            traceback.print_exception(e)
+            return
+        finally:
+            icmp_socket.close()
+
+        # TODO check source and destination addresses match
+
+        # parse Echo Reply
+        ip_header = reply_packet[:20]# first 20 bytes of reply packet is the IP header
+        version_and_ihl, tos, packet_len, identification, flags_and_offset, ttl, protocol, checksum, src_ip, dst_ip = struct.unpack('!BBHHHBBH4s4s', ip_header)
+
+        ip_header_len = (version_and_ihl & 0x0f) * 4# bit masking to extract IP header length
+
+        icmp_header = reply_packet[ip_header_len:ip_header_len + 8]
+        icmp_type, code, checksum, packet_id, sequence_num = struct.unpack('!BBHHH', icmp_header)
+
+        # TODO check ID and sequence numbers match
+
+        print('Reply received from %s' % (dst_addr))
+
+target_addr = sys.argv[1]
+ping = ICMPPing(target_addr)
 
 class NetworkScanner:
     # scan a single host and output address if host is active
@@ -67,7 +122,3 @@ class NetworkScanner:
         network = ipaddress.ip_network(network_addr, strict=False)
         for addr in network.hosts():
             self.scan_host(addr)
-
-network_addr = sys.argv[1]
-scanner = NetworkScanner()
-scanner.scan_network(network_addr)
